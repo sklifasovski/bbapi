6cc74f83f71121a1e64c504797b9ce45bb61f266
sklifasovski <36568927+sklifasovski@users.noreply.github.com>
2018-09-29 13:39:33 +0500

    Update 2gis.py

--- 2gis.py
+++ 2gis.py
@@ -3,8 +3,8 @@ import requests
 import pprint
 from urllib import parse
 
-question = input("Введите запрос: ")
-city_number = input("Введите номер города: ")
+question = input("Question: ")
+city_number = input("Number city: ")
 req_data_1 = {'region_id': city_number }
 req_data_2 = {'type':'street,adm_div.city,crossroad,adm_div.settlement,station,building,adm_div.district,road,adm_div.division,adm_div.region,adm_div.living_area,attraction,adm_div.place,adm_div.district_area,branch,parking,gate,route,foreign_city'}             
 req_data_3 = {'page': '1'}
